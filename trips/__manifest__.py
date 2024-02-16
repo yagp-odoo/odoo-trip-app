edd787b27ee62a7b396887eaa49a78007cfb884d
yagp-odoo <yagp@odoo.com>
2024-02-16 18:36:06 +0530

    [ADD] trips: Added required files to installed the app/module
    Added required initial files to installed the module like
    manifest,models,security...

---
+++ trips/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': "Trips",
+    'version': "0.1",
+    'depends': ["base"],
+    'author': "Yagnik (yagp)",
+    'category': "Travel",
+    'summary': "Organize and Manage All Your Travels with Odoo Trips",
+    'data': [
+
+    ],
+    'license': "LGPL-3",
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'description': """
+        Odoo Trips
+        ===============
+        Organize and Manage Your All Travels With Odoo Trips!!
+        Easy to Use!
+        Nice UI!
+        Accurate Caclulations!
+        Expense Tracking!
+        Participants, organizers and much more!!
+        """
+}
